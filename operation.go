642b6ef06cab647011a4c6d380145b64760dc25d
陈庄蕾 <chenzhuanglei@126.com>
2022-07-14 17:40:11 +0800

    add debug

--- operation.go
+++ operation.go
@@ -128,8 +128,7 @@ func (o *Operation) ioloop() {
 			// ignore quick and continous enter
 			if lastIsEnter && now-lastEnterTime < 1000 {
 				fmt.Fprintln(f, "enter to much 1")
-				o.buf.Refresh(nil) // to refresh the line
-				ignore = true      // ignore this rune
+				ignore = true // ignore this rune
 			}
 
 			lastEnterTime = now
